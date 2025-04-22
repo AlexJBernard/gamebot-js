e1169d56869b94f83b22ac365874f180ea5ffa3c
AlexJBernard <shrubventure@gmail.com>
2025-04-22 16:40:45 -0400

    FEATURE: Continued development of jsonDatabase and user class

--- gamebot/class/user.js
+++ gamebot/class/user.js
@@ -1,11 +1,83 @@
-
+/**
+ * Adds consistent functionality and structure for user data
+ */
 class User {
 
+  /**
+   * @type {String} The Discord member's username
+   */
+  username;
+
+  /**
+   * @type {Snowflake} The Discord member's userId
+   */
+  userId;
+
+  /**
+   * @type {Array<String>} String list of game names
+   */
   games = [];
 
+  /**
+   * 
+   * @param {String} username The username for the given Discord member
+   * @param {Snowflake} userId The userId for the given Discord member
+   * @param {Array} games List of games owned by this user
+   */
   constructor(username, userId, games) {
     this.username = username;
     this.userId = userId;
     this.games = games;
   }
+
+  /**
+   * @returns  A JSON Serializable object
+   */
+  get object() {
+    return {
+      username: this.username,
+      userId: this.userId,
+      games: this.games
+    }
+  }
+
+  /**
+   * @param {String} game The name of the game being added
+   */
+  set addGame(game) {
+    if (this.games.indexOf(game) >= 0) {
+      return false
+    }
+    
+    const newList = [...this.games, game]
+    this.games = newList
+    return true
+  }
+
+  /**
+   * @param {String} game Name of the game being removed
+   */
+  set removeGame(game) {
+    if (this.games.indexOf(game) < 0) {
+      return false
+    }
+
+    const newList = this.games.filter((item) => {
+      return item === game;
+    })
+    this.games = newList
+  }
+
+  /**
+   * 
+   * @param {String} game 
+   * @returns 
+   */
+  hasGame(game) {
+    return this.games.includes(game)
+  }
+
+
 }
+
+module.exports = User
--- gamebot/database/jsonDatabase.js
+++ gamebot/database/jsonDatabase.js
@@ -1,39 +1,75 @@
 
 const fs = require('fs');
+const User = require('../class/user')
 
-const fileUrl = 'data/data.json';
+const fileUrl = './data/data.json';
 
-getUserList = () => {
-  const readStream = fs.readFileSync('./data/data.json');
+/** 
+ * @returns {Array<User>} A list of User class objects
+*/
+const getUserList = () => {
+  const readStream = fs.readFileSync(fileUrl);
 
   const data = JSON.parse(readStream);
-  console.log(JSON.parse(fs.readFileSync('./data/data.json')))
+  /**
+   * @type {Array<User>} a list of 
+   */
+  let userData = []
+
+  return userData;
 }
 
 /**
- * @param {Array} updatedUserList
+ * @param {User} userData
  */
-updateUserList = (updatedUserList) => {
-  const writeStream = fs.writeFileSync('./data/data.json');
+const updateUserList = async (userData) => {
+  const userList = getUserList()
+  /**
+   * @type {Array<User.object>}
+   */
+  let output = []
+  userList.forEach((user) => {
+    output.push(user.object)
+  })
 
-  const newdata = JSON.stringify(updatedUserList)
+  fs.writeFileSync('./data/data.json', JSON.stringify(output),
+    {
+      encoding: "utf8",
+    }
+  );
 }
 
 module.exports = {
   getUser: function (userId) {
-    getUserList()
-    return null
+    const userList = getUserList()
+    return userList.find((user) => user.userId === userId)
   },
 
+  /**
+   * 
+   * @param {User} userData 
+   * @returns 
+   */
   saveUser: function(userData) {
     return null
   },
 
+  /**
+   * 
+   * @param {String} game 
+   * @returns 
+   */
   checkGame: function (game) {
     return null
   },
 
+  /**
+   * @param {Number} num The number of top games to show
+   */
   topGames: function(num) {
+    let topGameMap = newMap();
+    const userList = getUserList()
+
     return null;
   }
 }
